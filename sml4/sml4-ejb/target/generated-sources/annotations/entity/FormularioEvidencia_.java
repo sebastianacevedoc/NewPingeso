215db966adbcbc601f32ad41b6de4eb84c2e53a0
Aracelly Altamirano <arapuntocom@gmail.com>
2016-03-07 22:35:52 -0300

    proyecto generado durante la presentación con los profesores.

---
+++ sml4/sml4-ejb/target/generated-sources/annotations/entity/FormularioEvidencia_.java
@@ -0,0 +1,17 @@
+package entity;
+
+import entity.Evidencia;
+import entity.Formulario;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-07T12:23:58")
+@StaticMetamodel(FormularioEvidencia.class)
+public class FormularioEvidencia_ { 
+
+    public static volatile SingularAttribute<FormularioEvidencia, Formulario> formularioNUE;
+    public static volatile SingularAttribute<FormularioEvidencia, Evidencia> evidenciaidEvidencia;
+    public static volatile SingularAttribute<FormularioEvidencia, Integer> idFormularioEvidencia;
+
+}
---
+++ sml4/sml4-ejb/target/generated-sources/annotations/entity/Usuario_.java
@@ -0,0 +1,35 @@
+package entity;
+
+import entity.Area;
+import entity.Cargo;
+import entity.EdicionFormulario;
+import entity.Formulario;
+import entity.TipoUsuario;
+import entity.Traslado;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-07T12:23:58")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> passUsuario;
+    public static volatile ListAttribute<Usuario, Traslado> trasladoList;
+    public static volatile SingularAttribute<Usuario, TipoUsuario> tipoUsuarioidTipoUsuario;
+    public static volatile SingularAttribute<Usuario, Cargo> cargoidCargo;
+    public static volatile ListAttribute<Usuario, Traslado> trasladoList1;
+    public static volatile SingularAttribute<Usuario, Integer> idUsuario;
+    public static volatile ListAttribute<Usuario, Formulario> formularioList1;
+    public static volatile SingularAttribute<Usuario, String> cuentaUsuario;
+    public static volatile SingularAttribute<Usuario, String> apellidoUsuario;
+    public static volatile SingularAttribute<Usuario, Area> areaidArea;
+    public static volatile SingularAttribute<Usuario, String> rutUsuario;
+    public static volatile ListAttribute<Usuario, Formulario> formularioList;
+    public static volatile SingularAttribute<Usuario, String> mailUsuario;
+    public static volatile ListAttribute<Usuario, EdicionFormulario> edicionFormularioList;
+    public static volatile SingularAttribute<Usuario, Boolean> estadoUsuario;
+    public static volatile SingularAttribute<Usuario, String> nombreUsuario;
+
+}
